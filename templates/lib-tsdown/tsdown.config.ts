ab06c0548e751e641c4b2b94762c85f39c357aa1
royfuwei <royfuwei@gmail.com>
2026-01-06 21:02:06 +0800

    chore(lib-tsdown): update tsdown configuration
    Update the tsdown configuration file to align with latest project standards or formatting.

--- templates/lib-tsdown/tsdown.config.ts
+++ templates/lib-tsdown/tsdown.config.ts
@@ -25,8 +25,12 @@ export default defineConfig({
 
   treeshake: true,
 
-  // 對應原本 platform: 'neutral'
+  //
   platform: 'neutral',
+  // or
+  // platform: 'node',
+
+  fixedExtension: false,
 
   // 你原本沒設 target，就給個合理的預設
   target: 'es2023',
